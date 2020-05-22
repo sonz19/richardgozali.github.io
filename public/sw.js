5380c905dac487348e179db00776cd8a9db0cb0a
Richard Gozali <gozalirichard@gmail.com>
2020-05-22 21:22:47 +0700

    first time

---
+++ public/sw.js
@@ -0,0 +1,173 @@
+var CACHE_STATIC_NAME = 'static-v2';
+var CACHE_DYNAMIC_NAME = 'dynamic-v';
+
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+           './',
+          './index.html',
+          '/offline.html',
+          './src/js/app.js',
+          './src/js/feed.js',
+          './src/js/promise.js',
+          './src/js/fetch.js',
+          './src/js/material.min.js',
+          './src/css/animate.css',
+          './src/css/bootstrap.min.css',
+          './src/css/classy-nav.min.css',
+          './src/css/custom-icon.css',
+          './src/css/font-awesome.min.css',
+          './src/css/magnific-popup.css',
+          './src/css/nice-select.min.css',
+          './src/css/owl.carousel.min.css',
+          './src/images/main-image.jpg',
+          './src/fonts/classy.eot',
+          './src/fonts/classy.svg',
+          './src/fonts/classy.ttf',
+          './src/fonts/classy.woff',
+          './src/fonts/FontAwesome.otf',
+          './src/fonts/fontawesome-webfont.eot',
+          './src/fonts/fontawesome-webfont.svg',
+          './src/fonts/fontawesome-webfont.ttf',
+          './src/fonts/fontawesome-webfont.woff',
+          './src/fonts/fontawesome-webfont.woff2',
+          './src/fonts/icomoon.eot',
+          './src/fonts/icomoon.svg',
+          './src/fonts/icomoon.ttf',
+          './src/fonts/icomoon.woff',
+          './src/img/core-img/salad.png',
+          /*'/',*/
+         /* '/index.html',
+          '
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'*/
+        ]);
+      })
+  )
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  var url = 'https://httpbin.org/get';
+
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(function (cache) {
+          return fetch(event.request)
+            .then(function (res) {
+              cache.put(event.request, res.clone());
+              return res;
+            });
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(function (res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function (cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              })
+              .catch(function (err) {
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(function (cache) {
+                    return cache.match('/offline.html');
+                  });
+              });
+          }
+        })
+    );
+  }
+});
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 })
+//             })
+//             .catch(function(err) {
+//               return caches.open(CACHE_STATIC_NAME)
+//                 .then(function(cache) {
+//                   return cache.match('/offline.html');
+//                 });
+//             });
+//         }
+//       })
+//   );
+// });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(function(res) {
+//         return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 })
+//       })
+//       .catch(function(err) {
+//         return caches.match(event.request);
+//       })
+//   );
+// });
+
+// Cache-only
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
+// });
+
+// Network-only
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     fetch(event.request)
+//   );
+// });
